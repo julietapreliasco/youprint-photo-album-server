52e7e97b888ab34331c9e3f036425556dbc839e8
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-05-28 11:37:02 -0300

    YP-fix: Cors

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import router from './routes';
+import cors from 'cors'
 
 dotenv.config();
 
@@ -10,7 +11,8 @@ const port = process.env.PORT || 3000;
 
 const mongoURI = process.env.MONGODB_URI || '';
 
-mongoose.connect(mongoURI)
+mongoose
+  .connect(mongoURI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -18,12 +20,30 @@ mongoose.connect(mongoURI)
     console.error('Error connecting to MongoDB:', error);
   });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header(
+//     'Access-Control-Allow-Origin',
+//     process.env.NODE_ENV === 'production'
+//       ? 'https://youprint-photo-album.vercel.app'
+//       : 'http://localhost:5173',
+//   );
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+//   );
+//   next();
+// });
+
+// const corsOptions = {
+//   origin:
+//     process.env.NODE_ENV === 'production'
+//       ? 'https://youprint-photo-album.vercel.app'
+//       : 'http://localhost:5173',
+//   optionsSuccessStatus: 200,
+// };
+
+app.use(cors());
 
 app.use(express.json());
 
