14d3adb82155d4813e9c6122e0d111d890591e74
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-05-29 16:54:53 -0300

    YP-011: Edit photo album endpoint

--- api/controllers/photoAlbum.ts
+++ api/controllers/photoAlbum.ts
@@ -1,11 +1,11 @@
-import PhotoAlbumModel from '../models/photoAlbum';
+import PhotoAlbumModel from "../models/photoAlbum";
 
 export const createPhotoAlbum = async (req: any, res: any) => {
   try {
     const { photos, client } = req.body;
 
     if (!photos || !client.phone) {
-      return res.status(400).json({ error: 'Faltan datos requeridos' });
+      return res.status(400).json({ error: "Faltan datos requeridos" });
     }
 
     const newPhotoAlbum = new PhotoAlbumModel({
@@ -23,13 +23,12 @@ export const createPhotoAlbum = async (req: any, res: any) => {
   }
 };
 
-
-export const getPhotoAlbums = async (req:any, res:any) => {
+export const getPhotoAlbums = async (req: any, res: any) => {
   try {
     if (req.params.id) {
       const photoAlbum = await PhotoAlbumModel.findById(req.params.id);
       if (!photoAlbum) {
-        return res.status(404).json({ error: 'Álbum de fotos no encontrado' });
+        return res.status(404).json({ error: "Álbum de fotos no encontrado" });
       }
       res.status(200).json(photoAlbum);
     } else {
@@ -46,10 +45,37 @@ export const deletePhotoAlbum = async (req: any, res: any) => {
     const photoAlbumId = req.params.id;
     const photoAlbum = await PhotoAlbumModel.findById(photoAlbumId);
     if (!photoAlbum) {
-      return res.status(404).json({ error: 'Álbum de fotos no encontrado' });
+      return res.status(404).json({ error: "Álbum de fotos no encontrado" });
     }
     await PhotoAlbumModel.deleteOne({ _id: photoAlbumId });
-    res.status(200).json({ message: 'Álbum de fotos eliminado exitosamente' });
+    res.status(200).json({ message: "Álbum de fotos eliminado exitosamente" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const updatePhotoAlbum = async (req: any, res: any) => {
+  try {
+    const { id } = req.params;
+    const { photos } = req.body;
+
+    if (!photos) {
+      return res
+        .status(400)
+        .json({ error: "Error, debe especificar un nuevo orden de las fotos" });
+    }
+
+    const photoAlbum = await PhotoAlbumModel.findById(id);
+    if (!photoAlbum) {
+      return res.status(404).json({ error: "Álbum de fotos no encontrado" });
+    }
+
+    photoAlbum.photos = photos;
+    photoAlbum.updatedAt = new Date();
+
+    await photoAlbum.save();
+
+    res.status(200).json(photoAlbum);
   } catch (error) {
     res.status(500).json({ error: error });
   }
--- api/routes/photoAlbum.ts
+++ api/routes/photoAlbum.ts
@@ -1,9 +1,18 @@
-import express from 'express';
-import { createPhotoAlbum, deletePhotoAlbum, getPhotoAlbums } from '../controllers/photoAlbum';
+import express from "express";
+import {
+  createPhotoAlbum,
+  deletePhotoAlbum,
+  getPhotoAlbums,
+  updatePhotoAlbum,
+} from "../controllers/photoAlbum";
 
 const router = express.Router();
 
-router.route('/').get(getPhotoAlbums).post(createPhotoAlbum);
-router.route('/:id').get(getPhotoAlbums).delete(deletePhotoAlbum);
+router.route("/").get(getPhotoAlbums).post(createPhotoAlbum);
+router
+  .route("/:id")
+  .get(getPhotoAlbums)
+  .delete(deletePhotoAlbum)
+  .put(updatePhotoAlbum);
 
 export default router;
